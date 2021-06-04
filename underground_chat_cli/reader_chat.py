8e38eeb2b6281c101aeb80893089bbefe52b863f
Евгений <evgesha@pop-os.localdomain>
2021-06-04 20:54:34 +0300

    add requirements, exceptions, README

--- underground_chat_cli/reader_chat.py
+++ underground_chat_cli/reader_chat.py
@@ -2,16 +2,20 @@ import argparse
 import asyncio
 import datetime
 import os
+import socket
 
 import aiofiles
 from dotenv import load_dotenv
 
 
-async def main(host: str, port: int, file_history: str) -> None:
+async def main(host: str = None, port: int = None, file_history: str = None) -> None:
     try:
         reader, _ = await asyncio.open_connection(host, port)
-    except ConnectionRefusedError as e:
+    except (ConnectionRefusedError, socket.gaierror) as e:
         print(f'Error connection host {host}:{port}')
+        return
+    except TimeoutError as e:
+        print(f'Connection host {host}:{port} timeout')
         return
 
     async with aiofiles.open(file_history, mode='a') as f:
@@ -24,23 +28,26 @@ async def main(host: str, port: int, file_history: str) -> None:
             await f.write(f'{time_message}\n')
             await f.flush()
 
+
 if __name__ == "__main__":
     load_dotenv()
-    host = os.getenv('CHAT_HOST')
-    port = os.getenv('CHAT_PORT_READ')
-    file_history = os.getenv('FILE_HISTORY')
+    chat_host = os.getenv('CHAT_HOST')
+    chat_port = os.getenv('CHAT_PORT_READ')
+    chat_file_history = os.getenv('FILE_HISTORY')
 
     parser = argparse.ArgumentParser(description='Process some integers.')
 
-    parser.add_argument('--host', dest='host', type=str, default=host,
+    parser.add_argument('--host', dest='host', type=str, default=chat_host,
                         help='connection server host')
 
-    parser.add_argument('--port', dest='port', type=int, default=port,
+    parser.add_argument('--port', dest='port', type=int, default=chat_port,
                         help='connection server port')
 
-    parser.add_argument('--history', dest='history', type=str, default=file_history,
+    parser.add_argument('--history', dest='history', type=str, default=chat_file_history,
                         help='save history chat to file')
 
     args = parser.parse_args()
-
-    asyncio.run(main(args.host, args.port, file_history))
+    try:
+        asyncio.run(main(args.host, args.port, args.history))
+    except KeyboardInterrupt:
+        pass
--- underground_chat_cli/send_message.py
+++ underground_chat_cli/send_message.py
@@ -3,53 +3,60 @@ import asyncio
 import json
 import logging
 import os
-from types import coroutine
+import socket
+from typing import Optional, Callable
 
 from dotenv import load_dotenv
 
 logger = logging.getLogger('send_message')
 
 
-async def registration(send_data: coroutine, nickname: str) -> dict:
+async def registration(send_data: Callable, nickname: str) -> dict:
     await send_data('')
     result = await send_data(nickname)
     await send_data('')
     return json.loads(result)
 
 
-async def autorization(send_data: coroutine, token: str) -> bool:
-    result = await send_data(token)
+async def authorization(send_data: Callable, token_chat: Optional[str]) -> bool:
+    result = await send_data(token_chat)
     if result == 'null':
         print('Неизвестный токен. Проверьте его или зарегистрируйте заново.')
         return False
     return True
 
 
-def sender(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
-    async def message(write_data: str) -> str:
+def read_write_message(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> Callable:
+    async def wrapper(write_data: str) -> str:
         logger.debug(f'writer:  {write_data}')
         writer.write(f'{write_data}\n'.encode())
         fetch_message_raw = await reader.readline()
         fetch_message = fetch_message_raw.decode().strip()
         logger.debug(f"reader: {fetch_message}")
         return fetch_message
-    return message
+
+    return wrapper
 
 
-def submit_message(writer: asyncio.StreamWriter) -> None:
-    def message(message: str = '\n\n') -> None:
-        writer.write(f'{message}\n\n'.encode())
-        logger.debug(f'send_message:  {message}')
-    return message
+def submit_message(writer: asyncio.StreamWriter) -> Callable:
+    def wrapper(text_message: str = '\n\n') -> None:
+        writer.write(f'{text_message}\n\n'.encode())
+        logger.debug(f'send_message:  {text_message}')
+
+    return wrapper
 
 
 async def main(host: str = '', port: int = None, token: str = None, message: str = '', username: str = None) -> None:
     try:
         reader, writer = await asyncio.open_connection(host, port)
-    except ConnectionRefusedError as e:
+    except (ConnectionRefusedError, socket.gaierror) as e:
         print(f'Error connection host {host}:{port}')
-        return None
-    send_data = sender(reader, writer)
+        return
+    except TimeoutError as e:
+        print(f'Connection host {host}:{port} timeout')
+        return
+
+    send_data = read_write_message(reader, writer)
     await reader.readline()
 
     if username:
@@ -57,7 +64,7 @@ async def main(host: str = '', port: int = None, token: str = None, message: str
         print('Registration new user -', result_registration_user)
         return None
 
-    auth_result = await autorization(send_data, token)
+    auth_result = await authorization(send_data, token)
 
     if not auth_result:
         return None
@@ -68,9 +75,9 @@ async def main(host: str = '', port: int = None, token: str = None, message: str
 
 if __name__ == "__main__":
     load_dotenv()
-    host = os.getenv('CHAT_HOST')
-    port = os.getenv('CHAT_PORT_WRITE')
-    token = os.getenv('CHAT_TOKEN')
+    chat_host = os.getenv('CHAT_HOST')
+    chat_port = os.getenv('CHAT_PORT_WRITE')
+    chat_token = os.getenv('CHAT_TOKEN')
 
     log_format = '%(asctime)-15s %(message)s'
     logging.basicConfig(format=log_format)
@@ -78,12 +85,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Send message chat.')
 
     parser.add_argument('message', type=str, nargs='*', help='send message')
-    parser.add_argument('--host', dest='host', type=str, default=host,
+    parser.add_argument('--host', dest='host', type=str, default=chat_host,
                         help='connection server host')
 
-    parser.add_argument('--port', dest='port', type=int, default=port,
+    parser.add_argument('--port', dest='port', type=int, default=chat_port,
                         help='connection server port')
-    parser.add_argument('-t', '--token', dest='token', type=str, default=token,
+    parser.add_argument('-t', '--token', dest='token', type=str, default=chat_token,
                         help='connection server port')
     parser.add_argument('-d', '--debug', dest='loglevel', action='store_const', default=logging.INFO,
                         const=logging.DEBUG, help='Enable debug')
@@ -95,8 +102,10 @@ if __name__ == "__main__":
     if not args.message and not args.username:
         parser.error(
             'the following arguments are required: message or optional arguments -r(--registration)')
-    print(args)
 
     message = ' '.join(args.message)
-    asyncio.run(main(host=args.host, port=args.port,
-                token=args.token, message=message, username=args.username))
+    try:
+        asyncio.run(main(host=args.host, port=args.port,
+                     token=args.token, message=message, username=args.username))
+    except KeyboardInterrupt:
+        pass
